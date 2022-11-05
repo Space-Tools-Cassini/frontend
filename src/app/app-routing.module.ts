203095991c5259d39d72baf1eb3a8fcfdb6ca444
errezeeta <jramirezsamc@gmail.com>
2022-11-05 12:04:48 +0100

    Routing enabled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OutputComponent } from './output/output.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'output-component', component: OutputComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule,
