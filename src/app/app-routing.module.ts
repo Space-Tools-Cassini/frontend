a2a94555964aa33137b51970a55072d82c73bb72
errezeeta <jramirezsamc@gmail.com>
2022-11-05 16:38:57 +0100

    Routing solucionado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 import { OutputComponent } from './output/output.component';
 
 const routes: Routes = [
   { path: 'output-component', component: OutputComponent },
+  { path: '', component: MapComponent }
 ];
 
 @NgModule({
