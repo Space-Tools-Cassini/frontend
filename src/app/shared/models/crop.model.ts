6dd995f321a150a66ce84b3d8e7827f9f20aa353
as43z <albert.saez.nunez@estudiantat.upc.edu>
2022-11-06 01:16:20 +0100

    Finish crop datamodel

--- src/app/shared/models/crop.model.ts
+++ src/app/shared/models/crop.model.ts
@@ -4,13 +4,36 @@
  * Definiton of the crop datamodel.
  * 
  * **Ideal ranges**
- * @field ideal_temp  - Represents the range of the ideal temperature [number, number].
- * @field ideal_ph    - Represents the range of the ideal ph [number, number].
- * @field ideal_prep  - Represents the range of the ideal precipitation [number, number].
+ * @field ideal_temp  : Represents the range of the ideal temperature; [number, number].
+ * @field ideal_ph    : Represents the range of the ideal ph; [number, number].
+ * @field ideal_prep  : Represents the range of the ideal precipitation; [number, number].
  * 
+ * **Thresholds**
+ * @field thresh_temp : Represents the threshold temperature where if a value falls
+ *                      outside this values, the crop is droped; number.
+ * @field thresh_ph   : Represents the threshold ph where if a value falls
+ *                      outside this values, the crop is droped; number.
+ * @field thresh_prep : Represents the threshold precipitation where if a value falls
+ *                      outside this values, the crop is droped; number.
+ * 
+ * **Conditionals**
+ * @field bad_soil    : A crop can only grow in some soil types. This variable represents
+ *                      the list of soils that a crop cannot grow in, therefore, dropping
+ *                      it if this soil is present; string[].
+ * @field max_co2     : Maximum CO2 that a plant can take. From 0 to 100; number.
 */
 export interface Crop {
+    // Ideal ranges
     ideal_temp: [number, number];
     ideal_ph:   [number, number];
     ideal_prep: [number, number];
+
+    // Thresholds
+    thresh_temp: [number, number];
+    thresh_ph:   [number, number];
+    thresh_prep: [number, number];
+
+    // Conditionals
+    bad_soil: string[];
+    max_co2: number;
 }
